7ac9822f7d40d64170eba77b1fc1db48003a1cf8
unknown <amir.khanian.co@gmail.com>
2025-03-16 20:55:50 +0330

    add model db and users bluepoint

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Development
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config.from_object(Development)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
 def index():
     return "Blog Home"
+
+
+from mod_admin import admin
+from mod_users import users
+
+app.register_blueprint(users)
+app.register_blueprint(admin)
